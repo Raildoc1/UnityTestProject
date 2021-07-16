5620a0ac1aac633e1a4059aee8d806f975aeefb3
Raildoc1 <Kirill1056@list.ru>
2021-07-16 23:29:11 +0700

    hp bar

---
+++ Assets/_Project/Scripts/View/HPBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TestGame.Enemy;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestGame.View
+{
+    public class HPBar : MonoBehaviour
+    {
+        [SerializeField] private HealthManager _target;
+        [SerializeField] private Image _fillImage;
+
+        private void OnEnable()
+        {
+            if (!_target)
+            {
+                _target = GetComponentInParent<HealthManager>();
+            }
+
+            if (!_target)
+            {
+                Debug.LogError("HP Bar has no target!");
+                gameObject.SetActive(true);
+                return;
+            }
+
+            _target.OnHealthChanged += OnHealthChanged;
+        }
+
+        private void OnDisable()
+        {
+            if (!_target)
+            {
+                return;
+            }
+
+            _target.OnHealthChanged -= OnHealthChanged;
+        }
+
+        private void FixedUpdate()
+        {
+            transform.forward = (Camera.main.transform.position - transform.position).normalized;
+        }
+
+        private void OnHealthChanged(int current, int max)
+        {
+            _fillImage.fillAmount = current / (float)max;
+        }
+    }
+}
